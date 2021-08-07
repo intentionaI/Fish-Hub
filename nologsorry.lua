c01c5d3faac640c7224b24b711efc5d78d3cf880
nadic <82681098+nadic@users.noreply.github.com>
2021-08-07 15:57:09 +0300

    Update nologsorry.lua

--- nologsorry.lua
+++ nologsorry.lua
@@ -83,3 +83,22 @@ thread(function()
 end)
 
 print(syn.request({Url = "https://google.com"}))
+local keytab = {
+'Key3857235897585823752' ,
+'Key384723894273457235' ,
+'Key3457823985723857285' ,
+'Key35892538528543858347' ,
+'Key358757892523582' ,
+'Key234824274526527' ,
+'Key3588935825728572527' ,
+'Key322384238482384' ,
+'Key385729523952589' ,
+'Key58389458383485893458'
+}
+
+local deez = math.random(1, #keytab)
+local nondeez = keytab[deez]
+local lol = tostring(nondeez)
+print('Current key used is ' .. lol .. '!')
+
+_G.Key = lol
