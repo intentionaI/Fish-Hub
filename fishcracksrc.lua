736e88f8c93dd4246be9c8dc80bf08596335b71c
nadic <82681098+nadic@users.noreply.github.com>
2021-08-09 01:59:38 +0300

    Update fishcracksrc.lua

--- fishcracksrc.lua
+++ fishcracksrc.lua
@@ -1,4 +1,1 @@
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/nadic/Fish-Hub/main/nologsorry.lua'))()
-wait(2)
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/nadic/Fish-Hub/main/fishh2'))()
-
+project discontinued
